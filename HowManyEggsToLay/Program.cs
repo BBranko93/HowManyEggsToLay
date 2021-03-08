c21ece01d76cb867b564980e6a16c19f3aeaf248
BBranko93 <bane_b80@outlook.com>
2021-03-08 04:53:43 -0700

    Add project files.

---
+++ HowManyEggsToLay/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HowManyEggsToLay
+{
+    class Program
+    {
+        /// <summary>
+        /// Extending a base class
+        /// Using override
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Bird bird;
+            Console.Write("\nPress P for pigeon , O for ostrich: ");
+            char key = Char.ToUpper(Console.ReadKey().KeyChar);
+            if (key == 'P') bird = new Pigeon();
+            else if (key == 'O') bird = new Ostrich();
+            else return;
+
+            Console.Write("\nHow many eggs should it lay?");
+            if (!int.TryParse(Console.ReadLine(), out int numberOfEggs)) return;
+            Egg[] eggs = bird.LayEggs(numberOfEggs);
+            foreach (Egg egg in eggs)
+            {
+                Console.WriteLine(egg.Description);
+            }
+
+        }
+    }
+}
